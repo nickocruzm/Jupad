ae94c77974dafa82870e58121f67886d0cbaa6f1
Nicko Cruz M <nickocruzm@gmail.com>
2023-10-23 23:49:51 -0700

    feat: 60 sec timer

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random as rand
+from datetime import datetime
 import time
 
 score = 0
 
+def timeElapsed(t0):
+    t = datetime.now()
+    return( t.second - t0.second)
 
 def generateTest():
     i = 0
@@ -23,13 +27,21 @@ if __name__ == '__main__':
     test = ""
     
     #t0 = time.gmtime(time.gmtime())
+    start_time = time.time()
+    end_time = start_time + 50
     
-    while(usrInput == test):
+
+    
+    
+    while(0 <= (end_time - time.time()) ):
         test = generateTest()
-        userInput = input(f'{test}\n')
+        userInput = input(f'{test} ; \t {end_time - time.time()}\n')
         if(userInput == test):
             score += 1
     
+    print("score: ", score)
+
+
     # t1 = time.time()
     # print("score: ",score," ", (t1 - t0), " seconds")
         
