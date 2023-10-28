cd565b97a07cd7d6fe17f01b0c9f507cae01581c
Nicko Cruz M <nickocruzm@gmail.com>
2023-10-27 19:43:49 -0700

    fix: typo

--- display.py
+++ display.py
@@ -2,9 +2,10 @@ import pygame
 
 screen = pygame.display.set_mode((640, 240))
 running = True
+
 while running:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
-            running = false
+            running = False
 
 pygame.quit()
